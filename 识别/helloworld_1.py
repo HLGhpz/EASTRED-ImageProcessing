399d8ccb850afe49de48a85ddbd3b2f36a7e2e10
hlghpz <1090377416@qq.com>
2019-10-09 16:08:05 +0800

    mathematica使用lab来识别  效果没有使用rgb好

--- 识别/helloworld_1.py
+++ 识别/helloworld_1.py
@@ -7,7 +7,7 @@ from pyb import LED
 
 sensor.reset()                      # Reset and initialize the sensor.
 sensor.set_pixformat(sensor.RGB565) # Set pixel format to RGB565 (or GRAYSCALE)
-sensor.set_framesize(sensor.QVGA)   # Set frame size to QVGA (320x240)
+sensor.set_framesize(sensor.QQVGA)   # Set frame size to QVGA (320x240)
 sensor.skip_frames(time = 2000)     # Wait for settings take effect.
 clock = time.clock()                # Create a clock object to track the FPS.
 
@@ -19,9 +19,14 @@ while(True):
     led_red.on()
     led_blue.on()
     led_green.on()
-    clock.tick()                    # Update the FPS clock.
+    #clock.tick()                    # Update the FPS clock.
     img = sensor.snapshot()         # Take a picture and return the image.
+    clock.tick()
+    height = img.height()
+    width = img.width()
+    img.to_rgb565()
+
 
     print(clock.fps())              # Note: OpenMV Cam runs about half as fast when connected
                                     # to the IDE. The FPS should increase once disconnected.
-                                    sensor.RG
+
